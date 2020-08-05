810efae9dd37450fc9f228cf36e6df8f1c39f0aa
rolypolyvg295 <rolypolyvg295@users.noreply.github.com>
2020-08-05 13:11:57 +0900

    Update ui of element list and make buttons work

--- app/src/main/java/com/example/randomchooser/MainActivity.kt
+++ app/src/main/java/com/example/randomchooser/MainActivity.kt
@@ -2,24 +2,55 @@ package com.example.randomchooser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_element_list.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var randomCount = 1
+    private lateinit var recyclerViewAdapter: ElementListRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_element_list)
 
         val data = mutableListOf<Element>(Element("hello"), Element("bye"))
+        recyclerViewAdapter = ElementListRecyclerViewAdapter(this, data)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.element_list_recylerview)
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
+        element_list_recylerview.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = recyclerViewAdapter
+            addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+        }
 
-        val adapter = ElementListRecyclerViewAdapter(this, data)
-        recyclerView.adapter = adapter
+        element_list_plus_button.setOnClickListener(this)
+        element_list_minus_button.setOnClickListener(this)
+        element_list_add_button.setOnClickListener(this)
+        element_list_edit_button.setOnClickListener(this)
+        element_list_choose_random.setOnClickListener(this)
 
-        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        element_list_random_count.text = randomCount.toString()
+    }
+
+    override fun onClick(view: View) {
+        when (view.id){
+            R.id.element_list_plus_button -> {
+                if (randomCount < recyclerViewAdapter.itemCount)
+                randomCount++
+                element_list_random_count.text = randomCount.toString()
+            }
+            R.id.element_list_minus_button -> {
+                if (randomCount > 1)
+                    randomCount--
+
+                element_list_random_count.text = randomCount.toString()
+            }
+            R.id.element_list_add_button -> Toast.makeText(this, "add button", Toast.LENGTH_SHORT).show()
+            R.id.element_list_edit_button -> Toast.makeText(this, "edit button", Toast.LENGTH_SHORT).show()
+            R.id.element_list_choose_random -> Toast.makeText(this, "choose random button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
